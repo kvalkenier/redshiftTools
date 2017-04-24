9cf2fa9a1521359e227e7bd0e6c899539ec08e24
kwent <kwentakill@gmail.com>
2017-04-23 18:33:13 -0700

    Use IAM Role if AWS_IAM_ROLE_ARN is set

--- R/redshift-replace.R
+++ R/redshift-replace.R
@@ -34,13 +34,15 @@ rs_replace_table = function(
     bucket=Sys.getenv('AWS_BUCKET_NAME'),
     region=Sys.getenv('AWS_DEFAULT_REGION'),
     access_key=Sys.getenv('AWS_ACCESS_KEY_ID'),
-    secret_key=Sys.getenv('AWS_SECRET_ACCESS_KEY')
+    secret_key=Sys.getenv('AWS_SECRET_ACCESS_KEY'),
+    iam_role_arn=Sys.getenv('AWS_IAM_ROLE_ARN')
     )
   {
 
   Sys.setenv('AWS_DEFAULT_REGION'=region)
   Sys.setenv('AWS_ACCESS_KEY_ID'=access_key)
   Sys.setenv('AWS_SECRET_ACCESS_KEY'=secret_key)
+  Sys.setenv('AWS_IAM_ROLE_ARN'=iam_role_arn)
 
   if(missing(split_files)){
     print("Getting number of slices from Redshift")
@@ -59,13 +61,17 @@ rs_replace_table = function(
       queryDo(dbcon, sprintf("delete from %s", tableName))
 
       print("Copying data from S3 into Redshift")
-      queryDo(dbcon, sprintf("copy %s from 's3://%s/%s.' region '%s' csv gzip ignoreheader 1 emptyasnull COMPUPDATE FALSE credentials 'aws_access_key_id=%s;aws_secret_access_key=%s';",
+      copyStr = "copy %s from 's3://%s/%s.' region '%s' csv gzip ignoreheader 1 emptyasnull COMPUPDATE FALSE"
+      if (iam_role_arn) {
+        copyStr = paste(copyStr, sprintf("iam_role '%s'", iam_role_arn) sep=" "))
+      } else {
+        copyStr = paste(copyStr, sprintf("credentials 'aws_access_key_id=%s;aws_secret_access_key=%s'", access_key, secret_key) sep=" "))
+      }
+      queryDo(dbcon, sprintf(copyStr,
                           tableName,
                           bucket,
                           prefix,
-                          region,
-                          access_key,
-                          secret_key
+                          region
               ))
 
       print("Committing changes")
